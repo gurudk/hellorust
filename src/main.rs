6474faf94e2e7638e8ec784552f65aeb399d92af
gurudk <zhangyongji@126.com>
2023-10-11 17:31:39 +0800

    hello rust first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+
+fn greet_world(){
+    println!("Hello world");
+    let chn= "你好，rust";
+    let chn1 = "再次看见你，rust";
+    let li = [chn, chn1];
+    for l in li.iter(){
+        println!("{}", &l)
+    }
+
+}
+
+fn main(){
+    greet_world()
 }
