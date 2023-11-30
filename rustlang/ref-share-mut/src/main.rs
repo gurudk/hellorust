50209f99c18b1c1ee60d1e44652a5ca9c1145bba
gurudk <zhangyongji@126.com>
2023-11-30 19:49:56 +0800

    ref share mut

--- rustlang/ref-share-mut/src/main.rs
+++ rustlang/ref-share-mut/src/main.rs
@@ -14,9 +14,29 @@ fn main() {
         let tail = vec![3, 4];
         extend(&mut wave, &tail);
     }
-    extend(&mut wave, &wave);
+    // extend(&mut wave, &wave);
 
     println!("{:?}", wave);
+
+    let mut  x = 20;
+    let m1 = &mut x;
+    *m1 = 30;
+    let m2 = &mut x;
+    *m2 = 100;
+    println!("x={}", x);
+    // let m33 = &mut m2;
+    // **m33 = 333;
+    // println!("x33={}",x);
+
+    let mut v = (534,454);
+    let m = &mut v;
+    // let m1 = &mut v;
+    let m0 = &mut m.0;
+    //m.0 = 555;
+    println!("v1:{:?}", m.1);
+    *m0 = 666;
+    println!("v:{:?}", (*m).1);
+    println!("m0:{:?}", m0);
 
 }
 
