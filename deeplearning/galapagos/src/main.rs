1286e8dbe796af4162980d50ceab5a861debbcec
gurudk <zhangyongji@126.com>
2023-12-21 19:22:08 +0800

    init refactory

--- deeplearning/galapagos/src/main.rs
+++ deeplearning/galapagos/src/main.rs
@@ -1,50 +1,25 @@
-struct Variable<'a>{
-    data:Box<Option<f64>>,
-    creator:Box<Option<&'a mut dyn Function>>,
+struct Variable{
+    data:Option<f64>,
+    creator:Option<Box<dyn for<'c> Function<'c>>>,
 }
 
-trait Function{
+trait Function<'a>{
 
-    fn set_input(&mut self, inp:Box<Option<Variable>>);
-    fn get_input(&self)-> Box<Option<Variable>>;
-    fn set_output(&mut self, oup:Box<Option<Variable>>);
-    fn get_output(&self)-> Box<Option<Variable>>;
-    fn forward(&mut self);
-    fn backward(&self);
+    fn set_input<'b:'a>(&mut self, inp:Option<&'b Variable>);
+    fn get_input(&self)-> Box<Option<&Variable>>;
 }
 
 struct Square<'a>{
-    input:Box<Option<Variable<'a>>>,
-    output:Box<Option<Variable<'a>>>,
+    input:Option<&'a Variable>,
 }
 
-impl<'a> Function for Square<'a>{
-    fn get_input(&self)-> Box<Option<Variable>>{
-        self.input
-    }
-
-    fn get_output(&self)-> Box<Option<Variable>>{
-        self.output
-    }
-
-    fn set_input(&mut self, inp:Box<Option<Variable>>){
+impl<'a> Function<'a> for Square<'a>{
+    fn set_input<'b:'a>(&mut self, inp:Option<&'b Variable>){
         self.input = inp;
     }
 
-    fn set_output(&mut self, oup:Box<Option<Variable>>){
-        self.output = oup;
-    }
-
-    fn forward(&mut self){
-        let x = self.input.unwrap().data;
-        self.output = Box::new(Some(Variable{
-            data:Box::new(Some(x.unwrap()*x.unwrap())),
-            creator:Box::new(Some(self)),
-        }));
-    }
-
-    fn backward(&self){
-
+    fn get_input(&self)->Box<Option<&Variable>>{
+        Box::new(self.input)
     }
 }
 
