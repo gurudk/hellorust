c48df5f47bbebebde3bdef75072fb4f3981653aa
gurudk <zhangyongji@126.com>
2024-02-08 14:43:54 +0800

    passgen

--- algorithms/passgen/hash/src/lib.rs
+++ algorithms/passgen/hash/src/lib.rs
@@ -2,6 +2,8 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub mod merhash;
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +12,14 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    use crate::merhash::mersenne_hash;
+
+    #[test]
+    fn mersenne_hash_works() {
+        let seed = String::from("jdxjp");
+        let hash = mersenne_hash(&seed);
+        assert_eq!(2000375, hash);
     }
 }
