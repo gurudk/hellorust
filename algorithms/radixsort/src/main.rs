25f9fd0e8d6165f2ec3c628d353e6bdefe845555
gurudk <zhangyongji@126.com>
2024-03-10 19:32:31 +0800

    tim sort

--- algorithms/radixsort/src/main.rs
+++ algorithms/radixsort/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     // for i in 0..=0 {
     //     println!("000");
     // }
-    let mut arr: [i32; 20] = [0; 20];
+    let mut arr: [usize; 10] = [0; 10];
 
     let mut rng = rand::thread_rng();
     let uni = Uniform::from(1..100);
@@ -23,4 +23,46 @@ fn main() {
     println!("{}", 100usize.next_power_of_two());
 }
 
-fn radix_sort(data: &mut [i32]) {}
+fn radix_sort(data: &mut [usize]) {
+    if data.len() < 2 {
+        return;
+    }
+
+    let max_num = match data.iter().max() {
+        Some(&x) => x,
+        None => return,
+    };
+
+    let radix = data.len().next_power_of_two();
+
+    let mut digit: usize = 1;
+
+    println!("radix:{}", radix);
+    while digit <= max_num {
+        let index_of = |x| x / digit % radix;
+
+        let mut counter = vec![0; radix];
+        for &item in data.iter() {
+            counter[index_of(item)] += 1;
+        }
+
+        println!("counter:{:?}", counter);
+
+
+        for i in 1..radix {
+            counter[i] += counter[i - 1];
+        }
+
+        println!("counter:{:?}", counter);
+
+        //sort
+        for &item in data.to_owned().iter().rev() {
+            counter[index_of(item)] -= 1;
+            data[counter[index_of(item)]] = item;
+        }
+
+                println!("digit:{}, sorted data:{:?}", digit, data);
+
+        digit *= radix;
+    }
+}
---
+++ algorithms/timsort/src/main.rs
@@ -0,0 +1,179 @@
+use rand::distributions::{Distribution, Uniform};
+use std::cmp::min;
+
+fn main() {
+    println!("Hello, world!");
+
+    println!("{}", 7 >> 1);
+
+    // for i in 0..=0 {
+    //     println!("000");
+    // }
+    let mut arr: [i32; 20] = [0; 20];
+
+    let mut rng = rand::thread_rng();
+    let uni = Uniform::from(1..100);
+    for i in 0..arr.len() {
+        arr[i] = uni.sample(&mut rng);
+    }
+
+    let mut arr = [
+        20, 31, 37, 46, 68, 82, 88, 7, 16, 16, 19, 23, 80, 86, 6, 13, 20, 32, 52, 86,
+    ];
+
+    println!("{:?}", arr);
+    tim_sort(&mut arr);
+    // reverse_slice(&mut arr);
+
+    assert_eq!(insert_last(&mut [13, 72, 90]), &[13, 72, 90]);
+    println! {"ordered arr:{:?}",arr};
+}
+
+#[derive(Debug)]
+struct Batch {
+    start: usize,
+    end: usize,
+    size: usize,
+}
+
+impl Batch {
+    fn new(start: usize, end: usize) -> Self {
+        Batch {
+            start: start,
+            end: end,
+            size: end - start + 1,
+        }
+    }
+}
+
+const MIN_MERGE: usize = 64;
+
+fn tim_sort(data: &mut [i32]) {
+    if data.len() < 2 {
+        return;
+    }
+
+    if data.len() == 2 {
+        insert_last(data);
+        return;
+    }
+
+    //处理大于2个元素的情况
+
+    let minrun = cal_minrun(data.len());
+    /**** test minrun */
+    let minrun = 5;
+
+    let end = data.len();
+
+    let is_asc = |x, y| x < y;
+    let mut curr = 1;
+    let mut batch_start = curr - 1;
+    let mut batch_end = batch_start + 1;
+    let mut curr_asc = is_asc(data[curr - 1], data[curr]);
+    let mut batch_stack = Vec::new();
+
+    while batch_end < end - 1 {
+        batch_start = curr - 1;
+        batch_end = batch_start + 1;
+
+        //处理只有1个元素和两个元素的情况
+        if (end - batch_start) < 3 {
+            insert_last(&mut data[batch_start..]);
+            batch_stack.push(Batch::new(batch_start, end - 1));
+            break;
+        }
+
+        curr_asc = is_asc(data[curr - 1], data[curr]);
+
+        while (curr + 1 < end)
+            && (is_asc(data[curr - 1], data[curr]) == is_asc(data[curr], data[curr + 1]))
+        {
+            curr += 1;
+        }
+
+        batch_end = curr;
+        //如果是逆序，此时需要就地翻转
+        if !curr_asc {
+            reverse_slice(&mut data[batch_start..batch_end + 1]);
+        }
+
+        let batch_num = batch_end - batch_start + 1;
+        if batch_num < minrun {
+            let this_batch_end = min(end - 1, batch_start + minrun - 1);
+            for j in batch_end + 1..=this_batch_end {
+                //扩充当前批次
+                println!("before extend batch:{:?}", &data[batch_start..j + 1]);
+                insert_last(&mut data[batch_start..j + 1]);
+                println!("after extend batch:{:?}", &data[batch_start..j + 1]);
+            }
+            batch_end = this_batch_end;
+        }
+        //处理这个批次
+        batch_stack.push(Batch::new(batch_start, batch_end));
+
+        curr = batch_end + 2;
+    }
+
+    println!("batches:{:?}", batch_stack);
+}
+
+fn merge_batch(stack: &mut Vec<Batch>, curr_batch: Batch, data: &mut [i32]) {}
+
+fn reverse_slice(slice: &mut [i32]) {
+    if slice.len() < 2 {
+        return;
+    }
+
+    let last = slice.len() - 1;
+    let half = slice.len() >> 1;
+    for i in 0..half {
+        slice.swap(i, last - i);
+    }
+}
+
+fn insert_last(subseq: &mut [i32]) -> &mut [i32] {
+    if subseq.len() < 2 {
+        return subseq;
+    }
+
+    let last = subseq.len() - 1;
+    let subseq_end = last - 1;
+
+    if subseq[last] >= subseq[subseq_end] {
+        return subseq;
+    }
+
+    let mut found_pos = last;
+    for i in 0..=subseq_end {
+        if subseq[last] < subseq[i] {
+            found_pos = i;
+            break;
+        }
+    }
+
+    //insert last element to found_pos
+    if found_pos != last {
+        let temp = subseq[last];
+        for i in (found_pos..=subseq_end).rev() {
+            subseq[i + 1] = subseq[i];
+        }
+
+        //insert last elment to found pos
+        subseq[found_pos] = temp;
+    }
+
+    subseq
+}
+
+fn cal_minrun(len: usize) -> usize {
+    //calculate the minrun
+    let mut new_len = len;
+    let mut r = 0;
+    while new_len > MIN_MERGE {
+        r |= new_len & 1;
+        new_len >>= 1;
+    }
+
+    new_len + r
+}
